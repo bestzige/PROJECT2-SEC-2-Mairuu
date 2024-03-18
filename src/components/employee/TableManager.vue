9e66b30685e62b8ad03bae177dd75781fc3cf8a1
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2024-03-18 20:44:13 +0700

    Update TableManager.vue

--- src/components/employee/TableManager.vue
+++ src/components/employee/TableManager.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch } from 'vue'
 import { getItems } from '@/utils/fetch'
 import { API_ENDPOINT } from '@/utils/constants'
 const tables = ref()
@@ -35,28 +35,29 @@ watch(choosenTool, () => {
       <div class="w-7/12 bg-blue-200">
         <div class="w-full h-auto text-5xl text-center bg-blue-500">TABLES</div>
         <div class="flex flex-row gap-3 flex-wrap justify-around py-2">
-          <div
+          <button
             v-for="(table, index) in tables"
             :key="index"
-            class="text-6xl border-2 w-44 h-36 flex justify-center"
+            class="text-2xl border-2 w-44 h-36 flex justify-center"
             :class="choosenTable == table.id ? 'border-green-400' : ''"
             @click="choosenTable = table.id"
+            :disabled="table.status == 'available'"
           >
-            {{ table.id }}
-          </div>
+            {{ table.name.en }}
+          </button>
         </div>
       </div>
       <div class="w-5/12 bg-red-200 flex flex-col">
         <div class="w-full h-auto text-5xl text-center bg-red-500">MANAGER</div>
-        <div class="h-5/6 bg-slate-500">
+        <div class="h-5/6 bg-slate-500 text-2xl">
           <div v-if="choosenTool == 'order'">
             <div v-for="(order, index) in orders.items" :key="index" class="flex">
               {{ order }}
             </div>
           </div>
-          <div v-else-if="choosenTool == 'bill'">{{ orders.totalPrice }}</div>
+          <div v-else-if="choosenTool == 'bill'">{{ orders.totalPrice }} Baht</div>
         </div>
-        <div class="flex flex-row">
+        <div class="flex flex-row text-3xl">
           <button
             class="w-1/3 text-center border"
             :class="choosenTool == 'order' ? 'bg-green-400' : ''"
