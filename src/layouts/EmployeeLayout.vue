8d3f66c072cd43cd20c746ce8a79be2fac786ae2
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2024-03-20 09:12:04 +0700

    add router link in employee layout

--- src/layouts/EmployeeLayout.vue
+++ src/layouts/EmployeeLayout.vue
@@ -7,7 +7,7 @@ import DefaultLayout from './DefaultLayout.vue'
     <div
       class="fixed top-0 left-0 right-0 z-50 flex justify-between items-center p-4 bg-white shadow-md"
     >
-      <h1 class="text-md md:text-lg font-bold">Admin</h1>
+      <h1 class="text-md md:text-lg font-bold"><router-link to="/employee">Admin</router-link></h1>
     </div>
     <div class="flex flex-col h-full">
       <router-view />
