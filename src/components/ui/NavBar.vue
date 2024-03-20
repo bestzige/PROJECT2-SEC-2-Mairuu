0352bdc28593c4786d2c3ce6cb2e4dec34fe2300
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2024-03-20 13:54:37 +0700

    update

--- src/components/ui/NavBar.vue
+++ src/components/ui/NavBar.vue
@@ -1,13 +1,20 @@
 <script setup>
 import LangToggle from '@/components/lang/LangToggle.vue'
 import XToasts from './XToasts.vue'
+
+defineProps({
+  includeLangToggle: {
+    type: Boolean,
+    default: true
+  }
+})
 </script>
 
 <template>
   <div class="fixed top-0 left-0 right-0 z-50">
     <div class="flex justify-between items-center p-4 bg-white shadow-md">
       <slot />
-      <LangToggle />
+      <LangToggle v-if="includeLangToggle" />
     </div>
     <XToasts />
   </div>
