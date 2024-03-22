ad626955373242a9785286e799776c2aef5272d0
Perpasit <110712912+Perpasit@users.noreply.github.com>
2024-03-22 09:37:35 +0700

    Confirm delete modal

--- src/components/manager/TableDetail.vue
+++ src/components/manager/TableDetail.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineProps, defineEmits } from 'vue'
+import { ref } from 'vue'
 import { putItem } from '../../utils/fetch'
 
 const props = defineProps({
@@ -12,6 +12,7 @@ const props = defineProps({
 const isEditing = ref(false)
 const editedTable = ref({})
 const isDetailsOpen = ref(false)
+const showDeleteModal = ref(false)
 
 const toggleEditing = () => {
   isEditing.value = !isEditing.value
@@ -34,8 +35,17 @@ const saveItem = async () => {
   }
 }
 
-const deleteItem = () => {
+const deleteConfirmed = () => {
   emit('delete', props.table.id)
+  showDeleteModal.value = false
+}
+
+const confirmDelete = () => {
+  showDeleteModal.value = true
+}
+
+const cancelDelete = () => {
+  showDeleteModal.value = false
 }
 
 const toggleDetails = () => {
@@ -148,12 +158,36 @@ const toggleDetails = () => {
                 Save
               </button>
               <button
-                @click="deleteItem"
+                @click="confirmDelete"
                 class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 w-32"
               >
                 Delete
               </button>
             </div>
+          </div>
+        </div>
+      </div>
+      <!-- Delete Confirmation Modal -->
+      <div
+        v-if="showDeleteModal"
+        class="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50"
+      >
+        <div class="bg-white p-8 rounded shadow-md relative w-1/3">
+          <h2 class="text-2xl font-semibold mb-4">Confirm Delete</h2>
+          <p class="mb-4">Are you sure you want to delete this table?</p>
+          <div class="flex justify-end">
+            <button
+              @click="cancelDelete"
+              class="bg-gray-300 text-gray-800 px-4 py-2 rounded mr-2 hover:bg-gray-400"
+            >
+              Cancel
+            </button>
+            <button
+              @click="deleteConfirmed"
+              class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+            >
+              Delete
+            </button>
           </div>
         </div>
       </div>
